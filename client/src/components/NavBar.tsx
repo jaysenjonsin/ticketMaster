717a108d2e8d849319723546d84a82b80e524813
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-03-27 15:29:23 -0700

    fix navbar borders

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useLocation } from 'react-router';
 
 const NavBar = () => {
+  // get current path
+  const route = useLocation();
   return (
     <>
       <Navbar
@@ -21,14 +24,23 @@ const NavBar = () => {
             <Nav.Link
               href='/search'
               style={{
-                border: '1px solid white',
+                border:
+                  route.pathname === '/search' ? '1px solid white' : 'none',
                 borderRadius: '1rem',
                 color: 'white',
               }}
             >
               Search
             </Nav.Link>
-            <Nav.Link href='/favorites' style={{ color: 'white' }}>
+            <Nav.Link
+              href='/favorites'
+              style={{
+                border:
+                  route.pathname === '/favorites' ? '1px solid white' : 'none',
+                borderRadius: '1rem',
+                color: 'white',
+              }}
+            >
               Favorites
             </Nav.Link>
           </Nav>
@@ -39,3 +51,5 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+// 1px solid white',
